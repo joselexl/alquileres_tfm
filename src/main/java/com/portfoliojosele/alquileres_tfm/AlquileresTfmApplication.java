223e042b7cea68ec8fc304c9adfde0b78e3c03d2
José Antonio <josea.garcia.lorenzo@gmail.com>
2026-06-29 11:54:29 +0200

    Creando el proyecto y configurado application properties para el acceso a MYSQL

---
+++ src/main/java/com/portfoliojosele/alquileres_tfm/AlquileresTfmApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliojosele.alquileres_tfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquileresTfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquileresTfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfoliojosele/alquileres_tfm/AlquileresTfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliojosele.alquileres_tfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquileresTfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
