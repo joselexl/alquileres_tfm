1e1d60651c108c280e524ffed382638b53fb2151
José Antonio <josea.garcia.lorenzo@gmail.com>
2026-08-17 11:40:09 +0200

    Modificando inquilinorepository para incluir la interfaz crudrepository

--- src/main/java/com/portfoliojosele/alquileres_tfm/models/dao/InquilinoRepository.java
+++ src/main/java/com/portfoliojosele/alquileres_tfm/models/dao/InquilinoRepository.java
@@ -1,5 +1,11 @@
 package com.portfoliojosele.alquileres_tfm.models.dao;
+import org.springframework.data.repository.CrudRepository;
+import com.portfoliojosele.alquileres_tfm.models.entity.Inquilino;
 
-public class InquilinoRepository {
 
+public interface InquilinoRepository extends CrudRepository<Inquilino, Long> {
+    
+    // Al dejarlo vacío pero heredando de CrudRepository, 
+    // Spring Boot ya nos crea todos los métodos de base de datos por detrás.
+    
 }
