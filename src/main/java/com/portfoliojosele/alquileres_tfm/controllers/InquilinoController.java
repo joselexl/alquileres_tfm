baf4684bcfec8a9bd1efbc526265b9a27dc64236
José Antonio <josea.garcia.lorenzo@gmail.com>
2026-08-19 13:50:22 +0200

    añado crear en inquilino controller

--- src/main/java/com/portfoliojosele/alquileres_tfm/controllers/InquilinoController.java
+++ src/main/java/com/portfoliojosele/alquileres_tfm/controllers/InquilinoController.java
@@ -3,6 +3,8 @@ package com.portfoliojosele.alquileres_tfm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.portfoliojosele.alquileres_tfm.models.entity.Inquilino;
 import com.portfoliojosele.alquileres_tfm.models.services.InquilinoService;
 
 @Controller
@@ -20,5 +22,19 @@ public class InquilinoController {
         model.addAttribute("titulo", "Listado de inquilinos");
         model.addAttribute("inquilinos", inquilinoService.findAll());
         return "listar";
+    }
+
+    @GetMapping("/form")
+    public String crear(Model model) {
+        
+        // 1. Creamos la "caja vacía"
+        Inquilino inquilino = new Inquilino();
+        
+        // 2. La metemos en la mochila para el HTML
+        model.addAttribute("inquilino", inquilino);
+        model.addAttribute("titulo", "Formulario de Inquilino");
+        
+        // 3. Llamamos a la vista form.html
+        return "form";
     }
 }
