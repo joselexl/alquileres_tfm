b3d23f4f9bb18c746cb59629f765486a3112766e
José Antonio <josea.garcia.lorenzo@gmail.com>
2026-09-04 12:13:22 +0200

    creado FianzaController para poder tener los mapeos, recarga de listas para el CRUD

--- src/main/java/com/portfoliojosele/alquileres_tfm/models/services/FianzaService.java
+++ src/main/java/com/portfoliojosele/alquileres_tfm/models/services/FianzaService.java
@@ -1,5 +1,14 @@
 package com.portfoliojosele.alquileres_tfm.models.services;
+import java.util.List;
+import com.portfoliojosele.alquileres_tfm.models.entity.Fianza;
+
+
 
 public interface FianzaService {
+
+    public List<Fianza> findAll();
+    public void save(Fianza fianza);
+    public Fianza findOne(Long id);
+    public void delete(Long id);
 
 }
--- src/main/java/com/portfoliojosele/alquileres_tfm/models/services/FianzaServicesImpl.java
+++ src/main/java/com/portfoliojosele/alquileres_tfm/models/services/FianzaServicesImpl.java
@@ -1,5 +1,44 @@
 package com.portfoliojosele.alquileres_tfm.models.services;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.portfoliojosele.alquileres_tfm.models.dao.FianzaRepository;
+import com.portfoliojosele.alquileres_tfm.models.entity.Fianza;
 
-public class FianzaServicesImpl {
+
+
+@Service
+public class FianzaServicesImpl implements FianzaService {
+
+ private final FianzaRepository fianzaDao;
+
+    public FianzaServicesImpl(FianzaRepository fianzaDao) {
+        this.fianzaDao = fianzaDao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Fianza> findAll() {
+        // Le pedimos al repositorio de MySQL que nos devuelva todos las fianzas
+        return (List<Fianza>) fianzaDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void save(Fianza fianza) {
+        fianzaDao.save(fianza);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Fianza findOne(Long id) {
+        return fianzaDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        fianzaDao.deleteById(id);
+    }
 
 }
